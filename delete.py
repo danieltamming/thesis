395cc248057698dacf3189e0828df353d06ed2bb
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-01-21 15:43:59 -0500

    trying subj

--- delete.py
+++ delete.py
@@ -12,7 +12,7 @@ from agents.bert import BertAgent
 
 
 # thing = RnnAgent('foo', 'sst', 25, None, 'dev', 8)
-thing = BertAgent('foo', 'trec', 25, None, 'dev', 32)
+thing = BertAgent('foo', 'subj', 25, None, 'dev', 32)
 thing.run()
 exit()
 
