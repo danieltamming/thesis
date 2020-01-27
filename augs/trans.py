9006227417ccdc9bd5df404ead867030d4fd25ae
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-01-27 13:08:19 -0500

    adding translation script

--- augs/trans.py
+++ augs/trans.py
@@ -1,3 +1,8 @@
+import os
+import string
+from collections import Counter
+
+from tqdm import tqdm
 import torch
 
 class Translator():
@@ -26,3 +31,69 @@ class Translator():
 		# 	return self.ru2en.translate(self.en2ru.translate(example))
 		else:
 			raise ValueError('Unrecognized pivot language.')
+
+def is_english(s):
+	try:
+		s.encode(encoding='utf-8').decode('ascii')
+	except UnicodeDecodeError:
+		return False
+	else:
+		return True
+
+def gen_trans_aug(example, en2de, de2en, beam, temperature):
+	example_aug_list = []
+	en_bin = en2de.encode(example)
+	de_bin = en2de.generate(en_bin, beam=beam, sampling=True, 
+							temperature=temperature)
+	de_str_list = [en2de.decode(res['tokens']) for res in de_bin]
+	for de_str in de_str_list:
+		de_bin = de2en.encode(de_str)
+		en_bin = de2en.generate(de_bin, beam=beam, sampling=True, 
+								temperature=temperature)
+		example_aug_list.extend([de2en.decode(res['tokens']) for res in en_bin])
+	# return [s for s in example_aug_list if is_english(s)]
+	return example_aug_list
+
+def gen_save_trans(downloaded_dir, data_name, en2de, de2en):
+	if data_name == 'sst':
+		read_type = 'r'
+	else:
+		read_type = 'rb'
+	data_dir = os.path.join(downloaded_dir, data_name)
+	trans_aug_dir = os.path.join(data_dir, 'trans_aug')
+	for filename in os.listdir(data_dir):
+		no_aug_filepath = os.path.join(data_dir, filename)
+		if not os.path.isfile(no_aug_filepath):
+			continue
+		trans_aug_filepath = os.path.join(trans_aug_dir, filename)
+		with open(no_aug_filepath, read_type) as f, open(trans_aug_filepath, 'w') as g:
+			for line in tqdm(f.read().splitlines()):
+				if read_type == 'rb':
+					line = line.decode('latin-1')
+				label, example = line.split(maxsplit=1)
+				g.write(label+'\n')
+				g.write(example+'\n')
+				example_aug_list = gen_trans_aug(example, en2de, de2en, 5, 0.8)
+				safe = [s for s in example_aug_list if is_english(s)]
+				unsafe = [s for s in example_aug_list if not is_english(s)]
+				if len(safe) < 25:
+					print(example)
+					for s in unsafe:
+						print(s)
+				else:
+					print('Success!')
+				for example_aug, count in Counter(safe).most_common():
+					g.write(str(count) + ' ' + example_aug + '\n')
+				g.write('\n')
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+en2de = torch.hub.load(
+	'pytorch/fairseq', 'transformer.wmt19.en-de.single_model', 
+	tokenizer='moses', bpe='fastbpe').to(device)
+de2en = torch.hub.load(
+	'pytorch/fairseq', 'transformer.wmt19.de-en.single_model',
+	tokenizer='moses', bpe='fastbpe').to(device)
+
+downloaded_dir = '../DownloadedData/'
+for data_name in ['sst', 'subj', 'trec']:
+	gen_save_trans(downloaded_dir, data_name, en2de, de2en)
