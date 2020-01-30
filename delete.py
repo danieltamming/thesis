51bc4b4d532756adea5218b3d5fd804ec163c3ac
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-01-30 14:30:27 -0500

    more mp testing

--- delete.py
+++ delete.py
@@ -4,14 +4,12 @@ import time
 
 from utils.logger import initialize_logger
 
-def my_func(t, k):
+def my_func(t):
 	time.sleep(t)
-	return (t, k)
 
 print(mp.cpu_count())
 pool = mp.Pool(mp.cpu_count())
 start_time = time.time()
-results = pool.starmap(my_func, [(4, 1), (3, 2), (2, 3), (1, 4), (5, 5)])
+results = pool.map(my_func, list(range(1,6)))
 print(time.time() - start_time)
 pool.close()
-print(results)
