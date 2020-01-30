6a5d73699aaeea5423f89db465f4ac2282bada0c
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-01-30 14:27:01 -0500

    using delete.py to test multiprocessing

--- delete.py
+++ delete.py
@@ -4,17 +4,6 @@ import time
 
 from utils.logger import initialize_logger
 
-logger = initialize_logger(os.path.basename(__file__).split('.')[0])
-logger.info('Hello')
-time.sleep(3)
-logger = initialize_logger(os.path.basename(__file__).split('.')[0])
-logger.info('Goodbye')
-exit()
-print(os.path.basename(__file__).split('.')[0])
-time.sleep(3)
-initialize_logger('temp')
-exit()
-
 def my_func(t, k):
 	time.sleep(t)
 	return (t, k)
