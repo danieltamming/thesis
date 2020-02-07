f391f7695d835f86de53cdc7442342030d9ae714
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-02-07 09:12:51 -0500

    bug fix

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ import numpy as np
 from agents.rnn import RnnAgent
 from agents.bert import BertAgent
 from utils.logger import initialize_logger
-from utils.parsing import get_config
+from utils.parsing import get_device
 
-
-# config = get_config()
+device = get_device()
 this_script_name = os.path.basename(__file__).split('.')[0]
 num_epochs = 4
 aug_mode = None
 batch_size = 16
 for balance_seed in range(5):
 	logger = initialize_logger(this_script_name, balance_seed)
-	thing = BertAgent('foo', logger, 'sst', 25, num_epochs, 
+	thing = BertAgent(device, logger, 'sst', 25, num_epochs, 
 					  aug_mode, 'dev', batch_size, 
 					  small_label=None, small_prop=None,
 					  balance_seed=balance_seed, undersample=False,
