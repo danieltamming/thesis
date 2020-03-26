cb25491cb5d6b8e08366aacff1a488d6950753a8
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-03-26 11:58:17 -0400

    creating aug files

--- augs/context.py
+++ augs/context.py
@@ -336,13 +336,10 @@ def create_files(seed):
 				agent.augment()
 
 if __name__ == "__main__":
-	# print('Number of cpus: {}'.format(mp.cpu_count()))
-	# pool = mp.Pool(mp.cpu_count())
-	# pool.map(create_files, [6, 9])
-	# pool.close()
-	create_files(6)
-	create_files(9)
-
+	print('Number of cpus: {}'.format(mp.cpu_count()))
+	pool = mp.Pool(mp.cpu_count())
+	pool.map(create_files, [6, 9])
+	pool.close()
 
 '''
 READY TO RUN TRAIN THEN AUGMENT AGAIN
