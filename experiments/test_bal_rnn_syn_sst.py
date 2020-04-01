14e8e7d1b018e71af0cb4a3e5acccc299db068b7
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-04-01 13:09:20 -0400

    bug fix

--- experiments/test_bal_rnn_syn_sst.py
+++ experiments/test_bal_rnn_syn_sst.py
@@ -23,15 +23,15 @@ lr  = 0.001
 
 # map from small_prop to its corresponding optimal geo
 pct_geo_map = {
-	0.1: 6, 
-	0.2: 6, 
-	0.3: 7, 
-	0.4: 7, 
-	0.5: 7, 
-	0.6: 7, 
-	0.7: 8, 
-	0.8: 8, 
-	0.9: 9
+	0.1: 0.6, 
+	0.2: 0.6, 
+	0.3: 0.7, 
+	0.4: 0.7, 
+	0.5: 0.7, 
+	0.6: 0.7, 
+	0.7: 0.8, 
+	0.8: 0.8, 
+	0.9: 0.9
 }
 
 def experiment(balance_seed):
@@ -39,14 +39,6 @@ def experiment(balance_seed):
 	for small_prop in np.arange(0.1, 1.0, 0.1):
 		small_prop = round(small_prop, 2)
 		for small_label in [0, 1]:
-			for undersample in [False, True]:
-				agent = RnnAgent(device, logger, 'sst', 25, num_epochs, lr, 
-								 None, 'test', 128, 
-								 small_label=small_label, 
-								 small_prop=small_prop, 
-								 balance_seed=balance_seed, 
-								 undersample=undersample)
-				agent.run()
 			geo = pct_geo_map[small_prop]
 			agent = RnnAgent(device, logger, 'sst', 25, num_epochs, lr,
 							 'synonym', 'test', 128, 
@@ -55,6 +47,14 @@ def experiment(balance_seed):
 							 balance_seed=balance_seed, 
 							 geo=geo)
 			agent.run()
+			for undersample in [False, True]:
+				agent = RnnAgent(device, logger, 'sst', 25, num_epochs, lr, 
+								 None, 'test', 128, 
+								 small_label=small_label, 
+								 small_prop=small_prop, 
+								 balance_seed=balance_seed, 
+								 undersample=undersample)
+				agent.run()
 
 # experiment(0)
 print('Number of cpus: {}'.format(mp.cpu_count()))
