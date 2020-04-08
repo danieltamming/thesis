b517df0b6194be6a1fbbcb7d60751c7c30476782
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-04-08 10:50:45 -0400

    test bal rnn syn subj

--- experiments/test_bal_rnn_syn_subj.py
+++ experiments/test_bal_rnn_syn_subj.py
@@ -35,34 +35,35 @@ param_map = {
 }
 
 def experiment(balance_seed, split_num):
-	logger = initialize_logger(this_script_name, balance_seed, other=split_num)
-	# for small_prop in np.arange(0.1, 1.0, 0.1):
-	# 	small_prop = round(small_prop, 2)
-	# 	param_prop_map = param_map[small_prop]
-	# 	for small_label in [0, 1]:
-	# 		geo, num_epochs = param_prop_map['aug']
-	# 		geo = round(geo, 2)
-	# 		agent = RnnAgent(device, logger, 'subj', 25, num_epochs+1, lr,
-	# 						 'synonym', 'test', 128, 
-	# 						 small_label=small_label, 
-	# 						 small_prop=small_prop, 
-	# 						 balance_seed=balance_seed,
-	# 						 split_num=split_num, 
-	# 						 geo=geo)
-	# 		agent.run()
-	# 		for undersample in [False, True]:
-	# 			if undersample:
-	# 				num_epochs = param_prop_map['under']
-	# 			else:
-	# 				num_epochs = param_prop_map['over']
-	# 			agent = RnnAgent(device, logger, 'subj', 25, num_epochs+1, lr,
-	# 							 None, 'test', 128, 
-	# 							 small_label=small_label, 
-	# 							 small_prop=small_prop, 
-	# 							 balance_seed=balance_seed,
-	# 							 split_num=split_num, 
-	# 							 undersample=undersample)
-	# 			agent.run()
+	logger = initialize_logger(
+		this_script_name, balance_seed, other=split_num)
+	for small_prop in np.arange(0.1, 1.0, 0.1):
+		small_prop = round(small_prop, 2)
+		param_prop_map = param_map[small_prop]
+		for small_label in [0, 1]:
+			geo, num_epochs = param_prop_map['aug']
+			geo = round(geo, 2)
+			agent = RnnAgent(device, logger, 'subj', 25, num_epochs+1, lr,
+							 'synonym', 'test', 128, 
+							 small_label=small_label, 
+							 small_prop=small_prop, 
+							 balance_seed=balance_seed,
+							 split_num=split_num, 
+							 geo=geo)
+			agent.run()
+			for undersample in [False, True]:
+				if undersample:
+					num_epochs = param_prop_map['under']
+				else:
+					num_epochs = param_prop_map['over']
+				agent = RnnAgent(device, logger, 'subj', 25, num_epochs+1, lr,
+								 None, 'test', 128, 
+								 small_label=small_label, 
+								 small_prop=small_prop, 
+								 balance_seed=balance_seed,
+								 split_num=split_num, 
+								 undersample=undersample)
+				agent.run()
 
 
 try:
