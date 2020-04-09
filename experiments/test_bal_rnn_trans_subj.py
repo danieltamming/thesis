a09bf5d382a9065a3e8eda17e05d8227f818de16
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-04-09 10:48:09 -0400

    other half

--- experiments/test_bal_rnn_trans_subj.py
+++ experiments/test_bal_rnn_trans_subj.py
@@ -67,8 +67,8 @@ def experiment(balance_seed, split_num):
 
 try:
 	split_num_list = list(range(10))
-	seed_list = list(range(2))
-	# seed_list = [3]
+	# seed_list = list(range(2))
+	seed_list = [3]
 	params = list(itertools.product(seed_list, split_num_list))
 	pool = mp.Pool(mp.cpu_count())
 	pool.starmap(experiment, params)
