586349ff32217a94aee2fbc195c2a3ba5e51bc0b
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-04-12 10:31:55 -0400

    augs

--- augs/create_context.py
+++ augs/create_context.py
@@ -270,7 +270,7 @@ def create_subj_files(split_num):
 	data_name = 'subj'
 	# for small_label in [0, 1]:
 	for small_label in [0]:
-		L = np.arange(0.1, 0.4, 0.1)
+		L = np.arange(0.2, 1.0, 0.1)
 		for small_prop in L:
 			small_prop = round(small_prop, 1)
 			print(data_name, small_label, small_prop)
@@ -280,10 +280,10 @@ def create_subj_files(split_num):
 			agent.augment()
 
 if __name__ == "__main__":
-	try:
-		pool = mp.Pool(mp.cpu_count())
-		pool.map(create_subj_files, [8, 9])
-	finally:
-		pool.close()
-		pool.join()
-	# create_subj_files(3)
+	# try:
+	# 	pool = mp.Pool(mp.cpu_count())
+	# 	pool.map(create_subj_files, [8, 9])
+	# finally:
+	# 	pool.close()
+	# 	pool.join()
+	create_subj_files(6)
