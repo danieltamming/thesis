0293a4477f4c04e26d2bd9d70266d638c7b5e9d4
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-04-13 15:22:49 -0400

    context aug

--- augs/create_context.py
+++ augs/create_context.py
@@ -269,7 +269,7 @@ def create_sst_files(seed):
 	small_prop = None
 	lr = 5e-5
 	data_name = 'sst'
-	for pct_usage in np.arange(0.1, 1.0, 0.1):
+	for pct_usage in np.arange(0.5, 1.0, 0.1):
 		pct_usage = round(pct_usage, 1)
 		print(data_name, small_label, small_prop)
 		agent = BertAgent(lr, data_name, seed, pct_usage, 
@@ -284,7 +284,7 @@ def create_subj_files(split_num):
 	data_name = 'subj'
 	# for small_label in [0, 1]:
 	for small_label in [1]:
-		L = np.arange(0.2, 1.0, 0.1)
+		L = np.arange(0.5, 1.0, 0.1)
 		for small_prop in L:
 			small_prop = round(small_prop, 1)
 			print(data_name, small_label, small_prop)
@@ -294,10 +294,10 @@ def create_subj_files(split_num):
 			agent.augment()
 
 if __name__ == "__main__":
-	# try:
-	# 	pool = mp.Pool(mp.cpu_count())
-	# 	pool.map(create_sst_files, [0, 1, 2, 3])
-	# finally:
-	# 	pool.close()
-	# 	pool.join()
-	create_sst_files(4)
+	try:
+		pool = mp.Pool(mp.cpu_count())
+		pool.map(create_sst_files, [1, 2, 3])
+	finally:
+		pool.close()
+		pool.join()
+	# create_sst_files(4)
