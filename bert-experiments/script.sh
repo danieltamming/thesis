a6b4560994deebbe35341d9855061da4c164a23d
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-04-15 10:48:35 -0400

    bug fix

--- bert-experiments/script.sh
+++ bert-experiments/script.sh
@@ -3,9 +3,9 @@
 set -e
 gpu=0
 a=0
-b=4
+b=6
 
-for small_prop in $(seq 0.1 0.1 0.9)
+for small_prop in $(seq 0.1 0.2 0.9)
 do
 	for small_label in 0 1
 	do
@@ -13,7 +13,7 @@ do
 		do
 			python bert-experiments/bal_bert_trans_sst.py -a $a -b $b -g $gpu -p $small_prop -l $small_label -u $undersample
 		done
-		for geo in $(seq 0.1 0.1 0.9)
+		for geo in $(seq 0.1 0.2 0.9)
 		do
 			python bert-experiments/bal_bert_trans_sst.py -a $a -b $b -g $gpu -p $small_prop -l $small_label -q $geo
 		done
