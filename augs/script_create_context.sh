42b56da7d00da2720ff12156d2a688e945b1abd1
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-04-20 09:51:17 -0400

    augs/script_create_context.sh

--- augs/script_create_context.sh
+++ augs/script_create_context.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e
-gpu=1
-a=0
-b=6
+gpu=0
+a=7
+b=8
 
-for pct_usage in $(seq 0.1 0.1 1.0)
+for pct_usage in $(seq 0.2 0.1 1.0)
 do
 	python augs/create_context.py -a $a -b $b -g $gpu -u $pct_usage
 done
