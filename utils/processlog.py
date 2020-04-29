e6177b37836cd15129626dc7ec1faac8ad75b171
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-04-29 15:57:57 -0400

    organized logs

--- utils/processlog.py
+++ utils/processlog.py
@@ -169,7 +169,7 @@ def plot_imbalance_experiments():
 	# data_name = 'subj'
 	data_name = 'sfu'
 	filepath = 'logs/archived/valids/{}_{}_{}_{}.log'.format(setting, model, aug_mode, data_name)
-	filepath = 'logs/pct_rnn_syn_sfu/temp.log'
+	filepath = 'logs/pct_rnn_trans_sfu/temp_2.log'
 	err_bars = False
 	experiments = read_experiments(filepath, avg_across_labels, setting)
 
