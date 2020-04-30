86dd640cb8997d3688cdd404f24afdcaefd1b5dc
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-04-30 19:50:46 -0400

    more tests

--- experiments/pct_rnn_syn_sfu.py
+++ experiments/pct_rnn_syn_sfu.py
@@ -19,12 +19,13 @@ from utils.parsing import get_device
 device = get_device()
 this_script_name = os.path.basename(__file__).split('.')[0]
 num_epochs = 100
-lr  = 0.01
+lr  = 0.005
 input_length = 128
 def experiment(balance_seed, split_num):
 	logger = initialize_logger(
 		this_script_name, balance_seed, other=split_num)
-	for pct_usage in np.arange(0.2, 1.1, 0.2):
+	# for pct_usage in np.arange(0.2, 1.1, 0.2):
+	for pct_usage in [0.2]:
 		pct_usage = round(pct_usage, 2)
 		agent = RnnAgent(device, logger, 'sfu', input_length, num_epochs, lr,
 						 None, 'dev', 128, 
