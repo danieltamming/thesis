82b6e0e20b107a2df7aac4f023d761a33244390b
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-05-04 09:53:43 -0400

    pct_script

--- bert-experiments/pct_script.sh
+++ bert-experiments/pct_script.sh
@@ -5,10 +5,10 @@ gpu=1
 a=0
 b=6
 
-for pct_usage in $(seq 0.3 0.2 0.7)
+for pct_usage in $(seq 0.2 0.2 0.8)
 do
 	python bert-experiments/pct_bert_trans_sst.py -a $a -b $b -g $gpu -p $pct_usage
-	for geo in $(seq 0.3 0.2 0.5)
+	for geo in $(seq 0.1 0.2 0.9)
 	do
 		python bert-experiments/pct_bert_trans_sst.py -a $a -b $b -g $gpu -p $pct_usage -q $geo
 	done
