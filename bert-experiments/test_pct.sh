3c21a2177db150d24577326d6bb661d73e3e4678
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-05-27 10:38:14 -0400

    logs from remote

--- bert-experiments/test_pct.sh
+++ bert-experiments/test_pct.sh
@@ -2,17 +2,17 @@
 
 set -e
 gpu=0
-# aug_mode="synonym"
+aug_mode="context"
 
-script="bert-experiments/test_bert_sst.py"
+# script="bert-experiments/test_bert_sst.py"
 # script="bert-experiments/test_bert_subj.py"
 # script="bert-experiments/test_bert_sfu.py"
 
-for aug_mode in "synonym" "trans" "context"
+for script in "bert-experiments/test_bert_sst.py" "bert-experiments/test_bert_subj.py"
 do
-	for a in $(seq 0 6 24)
+	for a in 0 5
 	do
-		(( b = a + 6 ))
+		(( b = a + 5 ))
 		for pct_usage in $(seq 0.2 0.2 1.0)
 		do
 			python $script -a $a -b $b -g $gpu -m $aug_mode -p $pct_usage -o
