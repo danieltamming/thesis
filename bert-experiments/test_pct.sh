c881ad34c60fd0b8eefe49859d6cc433c10eed76
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-05-27 16:19:56 -0400

    da

--- bert-experiments/test_pct.sh
+++ bert-experiments/test_pct.sh
@@ -2,7 +2,7 @@
 
 set -e
 gpu=0
-# aug_mode="synonym"
+# aug_mode="context"
 
 # script="bert-experiments/test_bert_sst.py"
 # script="bert-experiments/test_bert_subj.py"
@@ -10,9 +10,9 @@ script="bert-experiments/test_bert_sfu.py"
 
 for aug_mode in "synonym" "trans"
 do
-	for a in $(seq 0 6 24)
+	for a in $(seq 0 2 28)
 	do
-		(( b = a + 6 ))
+		(( b = a + 2 ))
 		for pct_usage in $(seq 0.2 0.2 1.0)
 		do
 			python $script -a $a -b $b -g $gpu -m $aug_mode -p $pct_usage -o
@@ -22,12 +22,12 @@ do
 done
 
 aug_mode="context"
-for a in 0 5
+for a in $(seq 0 2 8)
 do
-	(( b = a + 5 ))
+	((b = a + 2))
 	for pct_usage in $(seq 0.2 0.2 1.0)
 	do
-		python $script -a $a -b $b -g $gpu -m $aug_mode -p $pct_usage -o
 		python $script -a $a -b $b -g $gpu -m $aug_mode -p $pct_usage
+		python $script -a $a -b $b -g $gpu -m $aug_mode -p $pct_usage -o
 	done
 done
