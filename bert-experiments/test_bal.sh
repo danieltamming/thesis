911868eef08c09708b92150a2d84ed3d2dcd727e
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-05-29 15:22:18 -0400

    a

--- bert-experiments/test_bal.sh
+++ bert-experiments/test_bal.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
 set -e
-gpu=0
-aug_mode="trans"
+gpu=1
 
-script="bert-experiments/test_bert_sst.py"
-# script="bert-experiments/test_bert_subj.py"
+# script="bert-experiments/test_bert_sst.py"
+script="bert-experiments/test_bert_subj.py"
 # script="bert-experiments/test_bert_sfu.py"
 
 
+aug_mode="synonym"
+a=24
+b=30
+for small_prop in $(seq 0.2 0.2 0.8)
+do
+	for small_label in 0 1
+	do
+		for undersample in 0 1
+		do
+			python $script -a $a -b $b -g $gpu -m $aug_mode -r $small_prop -l $small_label -u $undersample -o
+		done
+		python $script -a $a -b $b -g $gpu -m $aug_mode -r $small_prop -l $small_label
+	done
+done
 
+
+aug_mode="trans"
 for a in $(seq 6 6 24)
 do
 	(( b = a + 6 ))
